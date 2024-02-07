1b008c72c321ec97c709afacb983c45071f0534a
raymond <raymondezra276@gmail.com>
2024-02-07 12:58:14 +0300

    initial

---
+++ app/api/topics/route.js
@@ -0,0 +1,26 @@
+import { NextResponse } from "next/server";
+import connectMongoDB from "../../../lib/MongoDb";
+import Topic from "../../../models/topic";
+
+export async function POST(request){
+    const { title, description } = await request.json();
+    await connectMongoDB();
+    await Topic.create({ title, description });
+    return NextResponse.json({message:'Topic created'}, { status: 201 })
+}
+
+export async function GET(){
+    await connectMongoDB();
+    const topics = await Topic.find();
+    return NextResponse.json({ topics })
+}
+
+export async function DELETE(request){
+    const id = request.nextUrl.searchParams.get('id');
+    await connectMongoDB();
+    await Topic.findByIdAndDelete(id);
+    return NextResponse.json({ message: 'Topic Deleted'}, { status : 200})
+}
+
+
+
